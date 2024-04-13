bd31619e4aef56f6ca8583f99bfec4234ac07cb6
Thibaut Durand <durand.tibo@gmail.com>
2024-04-13 12:15:27 -0700

    Add `cumprod/sum_along_batch/seq` (#20)

--- src/batcharray/array/__init__.py
+++ src/batcharray/array/__init__.py
@@ -5,9 +5,13 @@ from __future__ import annotations
 __all__ = [
     "concatenate_along_batch",
     "concatenate_along_seq",
-    "tile_along_seq",
+    "cumprod_along_batch",
+    "cumprod_along_seq",
+    "cumsum_along_batch",
+    "cumsum_along_seq",
     "take_along_batch",
     "take_along_seq",
+    "tile_along_seq",
 ]
 
 from batcharray.array.indexing import take_along_batch, take_along_seq
@@ -15,4 +19,10 @@ from batcharray.array.joining import (
     concatenate_along_batch,
     concatenate_along_seq,
     tile_along_seq,
+)
+from batcharray.array.math import (
+    cumprod_along_batch,
+    cumprod_along_seq,
+    cumsum_along_batch,
+    cumsum_along_seq,
 )
