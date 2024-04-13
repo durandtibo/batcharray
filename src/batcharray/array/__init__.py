de705dbd334cedb4b04ae485abb3d05d243bff0a
Thibaut Durand <durand.tibo@gmail.com>
2024-04-12 20:41:07 -0700

    Add `take_along_batch/seq`

---
+++ src/batcharray/array/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain functions to manipulate arrays."""
+
+from __future__ import annotations
+
+__all__ = ["take_along_batch", "take_along_seq"]
+
+from batcharray.array.indexing import take_along_batch, take_along_seq
