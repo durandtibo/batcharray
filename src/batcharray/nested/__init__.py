373cec9943efda5d0d7f24cdb142eb876265f34b
Thibaut Durand <durand.tibo@gmail.com>
2024-04-14 12:10:13 -0700

    Add `nested.concatenate_along_batch/seq`

--- src/batcharray/nested/__init__.py
+++ src/batcharray/nested/__init__.py
@@ -2,6 +2,17 @@ r"""Contain functions to manipulate nested data."""
 
 from __future__ import annotations
 
-__all__ = ["take_along_batch", "take_along_seq"]
+__all__ = [
+    "concatenate_along_batch",
+    "concatenate_along_seq",
+    "tile_along_seq",
+    "take_along_batch",
+    "take_along_seq",
+]
 
 from batcharray.nested.indexing import take_along_batch, take_along_seq
+from batcharray.nested.joining import (
+    concatenate_along_batch,
+    concatenate_along_seq,
+    tile_along_seq,
+)
