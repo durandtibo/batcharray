54aec09e3f052d2a431ede5e4423decd676f3868
Thibaut Durand <durand.tibo@gmail.com>
2024-04-17 13:53:46 -0700

    Add nested pointwise functions

--- src/batcharray/nested/__init__.py
+++ src/batcharray/nested/__init__.py
@@ -3,14 +3,16 @@ r"""Contain functions to manipulate nested data."""
 from __future__ import annotations
 
 __all__ = [
-    "arccosh",
+    "abs",
     "arccos",
+    "arccosh",
     "arcsin",
     "arcsinh",
     "arctan",
     "arctanh",
     "chunk_along_batch",
     "chunk_along_seq",
+    "clip",
     "concatenate_along_batch",
     "concatenate_along_seq",
     "cos",
@@ -19,6 +21,13 @@ __all__ = [
     "cumprod_along_seq",
     "cumsum_along_batch",
     "cumsum_along_seq",
+    "exp",
+    "exp2",
+    "expm1",
+    "log",
+    "log10",
+    "log1p",
+    "log2",
     "permute_along_batch",
     "permute_along_seq",
     "select_along_batch",
@@ -55,6 +64,17 @@ from batcharray.nested.permutation import (
     permute_along_seq,
     shuffle_along_batch,
     shuffle_along_seq,
+)
+from batcharray.nested.pointwise import (
+    abs,
+    clip,
+    exp,
+    exp2,
+    expm1,
+    log,
+    log1p,
+    log2,
+    log10,
 )
 from batcharray.nested.slicing import (
     chunk_along_batch,
---
+++ tests/unit/nested/test_pointwise.py
@@ -0,0 +1,51 @@
+from collections.abc import Callable
+from functools import partial
+
+import numpy as np
+import pytest
+from coola import objects_are_equal
+
+from batcharray import nested
+
+DTYPES = [np.float32, np.float64, np.int64]
+POINTWISE_FUNCTIONS = [
+    (np.abs, nested.abs),
+    (partial(np.clip, a_min=2, a_max=None), partial(nested.clip, a_min=2)),
+    (partial(np.clip, a_min=None, a_max=6), partial(nested.clip, a_max=6)),
+    (partial(np.clip, a_min=2, a_max=6), partial(nested.clip, a_min=2, a_max=6)),
+    (np.exp, nested.exp),
+    (np.exp2, nested.exp2),
+    (np.expm1, nested.expm1),
+    (np.log, nested.log),
+    (np.log2, nested.log2),
+    (np.log10, nested.log10),
+    (np.log1p, nested.log1p),
+]
+
+
+@pytest.mark.parametrize("dtype", DTYPES)
+@pytest.mark.parametrize("functions", POINTWISE_FUNCTIONS)
+def test_pointwise_function_array(dtype: np.dtype, functions: tuple[Callable, Callable]) -> None:
+    np_fn, nested_fn = functions
+    array = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]], dtype=dtype)
+    assert objects_are_equal(nested_fn(array), np_fn(array))
+
+
+@pytest.mark.parametrize("dtype", DTYPES)
+@pytest.mark.parametrize("functions", POINTWISE_FUNCTIONS)
+def test_cumprod_along_batch_dict(dtype: np.dtype, functions: tuple[Callable, Callable]) -> None:
+    np_fn, nested_fn = functions
+    assert objects_are_equal(
+        nested_fn(
+            {
+                "a": np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]], dtype=dtype),
+                "b": np.array([4, 3, 2, 1, 0], dtype=np.float32),
+                "c": [np.array([5, 6, 7, 8, 9], dtype=np.float64)],
+            },
+        ),
+        {
+            "a": np_fn(np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]], dtype=dtype)),
+            "b": np_fn(np.array([4, 3, 2, 1, 0], dtype=np.float32)),
+            "c": [np_fn(np.array([5, 6, 7, 8, 9], dtype=np.float64))],
+        },
+    )
