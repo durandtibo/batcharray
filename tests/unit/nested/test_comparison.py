6779252d7b6d24dfc2a062b60b5c3f84753d2fc0
Thibaut Durand <durand.tibo@gmail.com>
2024-04-19 23:21:59 -0700

    Add tests for nested `argsort` and `sort`

--- tests/unit/nested/test_comparison.py
+++ tests/unit/nested/test_comparison.py
@@ -46,12 +46,23 @@ def test_argsort_along_batch_nested() -> None:
                 "a": np.array([[2, 6], [0, 3], [4, 9], [8, 1], [5, 7]]),
                 "b": np.array([4, 3, 2, 1, 0], dtype=np.float32),
                 "list": [np.array([5, 6, 7, 8, 9])],
+                "masked": np.ma.masked_array(
+                    data=np.array([[3, 5, 0, 2, 4], [4, 7, 8, 8, 5], [8, 4, 1, 3, 0]]),
+                    mask=np.array(
+                        [
+                            [False, False, False, False, True],
+                            [False, False, False, True, False],
+                            [False, False, True, False, False],
+                        ]
+                    ),
+                ),
             }
         ),
         {
             "a": np.array([[1, 3], [0, 1], [2, 0], [4, 4], [3, 2]]),
             "b": np.array([4, 3, 2, 1, 0]),
             "list": [np.array([0, 1, 2, 3, 4])],
+            "masked": np.array([[0, 2, 0, 0, 2], [1, 0, 1, 2, 1], [2, 1, 2, 1, 0]]),
         },
     )
 
@@ -109,12 +120,23 @@ def test_argsort_along_seq_nested() -> None:
                 "a": np.array([[7, 3, 0, 8, 5], [1, 9, 6, 4, 2]]),
                 "b": np.array([[4, 3, 2, 1, 0]], dtype=np.float32),
                 "list": [np.array([[5, 6, 7, 8, 9]])],
+                "masked": np.ma.masked_array(
+                    data=np.array([[3, 5, 0, 2, 4], [4, 7, 8, 8, 5], [8, 5, 8, 8, 0]]),
+                    mask=np.array(
+                        [
+                            [False, False, False, False, True],
+                            [False, False, False, True, False],
+                            [False, False, True, False, False],
+                        ]
+                    ),
+                ),
             }
         ),
         {
             "a": np.array([[2, 1, 4, 0, 3], [0, 4, 3, 2, 1]]),
             "b": np.array([[4, 3, 2, 1, 0]]),
             "list": [np.array([[0, 1, 2, 3, 4]])],
+            "masked": np.array([[2, 3, 0, 1, 4], [0, 4, 1, 2, 3], [4, 1, 0, 3, 2]]),
         },
     )
 
@@ -172,12 +194,32 @@ def test_sort_along_batch_nested() -> None:
                 "a": np.array([[4, 9], [1, 7], [2, 5], [5, 6], [3, 8]]),
                 "b": np.array([4, 3, 2, 1, 0], dtype=np.float32),
                 "list": [np.array([5, 6, 7, 8, 9])],
+                "masked": np.ma.masked_array(
+                    data=np.array([[3, 5, 0, 2, 4], [4, 7, 8, 8, 5], [8, 5, 8, 8, 0]]),
+                    mask=np.array(
+                        [
+                            [False, False, False, False, True],
+                            [False, False, False, True, False],
+                            [False, False, True, False, False],
+                        ]
+                    ),
+                ),
             }
         ),
         {
             "a": np.array([[1, 5], [2, 6], [3, 7], [4, 8], [5, 9]]),
             "b": np.array([0, 1, 2, 3, 4], dtype=np.float32),
             "list": [np.array([5, 6, 7, 8, 9])],
+            "masked": np.ma.masked_array(
+                data=np.array([[3, 5, 0, 2, 0], [4, 5, 8, 8, 5], [8, 7, 8, 8, 4]]),
+                mask=np.array(
+                    [
+                        [False, False, False, False, False],
+                        [False, False, False, False, False],
+                        [False, False, True, True, True],
+                    ]
+                ),
+            ),
         },
     )
 
@@ -235,12 +277,32 @@ def test_sort_along_seq_nested() -> None:
                 "a": np.array([[4, 1, 2, 5, 3], [9, 7, 5, 6, 8]]),
                 "b": np.array([[4, 3, 2, 1, 0]], dtype=np.float32),
                 "list": [np.array([[5, 6, 7, 8, 9]])],
+                "masked": np.ma.masked_array(
+                    data=np.array([[3, 5, 0, 2, 4], [4, 7, 8, 8, 5], [8, 5, 8, 8, 0]]),
+                    mask=np.array(
+                        [
+                            [False, False, False, False, True],
+                            [False, False, False, True, False],
+                            [False, False, True, False, False],
+                        ]
+                    ),
+                ),
             }
         ),
         {
             "a": np.array([[1, 2, 3, 4, 5], [5, 6, 7, 8, 9]]),
             "b": np.array([[0, 1, 2, 3, 4]], dtype=np.float32),
             "list": [np.array([[5, 6, 7, 8, 9]])],
+            "masked": np.ma.masked_array(
+                data=np.array([[0, 2, 3, 5, 4], [4, 5, 7, 8, 8], [0, 5, 8, 8, 8]]),
+                mask=np.array(
+                    [
+                        [False, False, False, False, True],
+                        [False, False, False, False, True],
+                        [False, False, False, False, True],
+                    ]
+                ),
+            ),
         },
     )
 
