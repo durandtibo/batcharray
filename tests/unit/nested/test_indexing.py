02cbfec3130cf86c2216329b694a29b486c3dac7
Thibaut Durand <durand.tibo@gmail.com>
2024-04-20 22:10:55 -0700

    Add `nested.masked_select_along_batch/seq` (#67)

--- tests/unit/nested/test_indexing.py
+++ tests/unit/nested/test_indexing.py
@@ -9,6 +9,8 @@ from coola import objects_are_equal
 from batcharray.nested import (
     index_select_along_batch,
     index_select_along_seq,
+    masked_select_along_batch,
+    masked_select_along_seq,
     take_along_batch,
     take_along_seq,
 )
@@ -91,6 +93,74 @@ def test_index_select_along_seq_dict(dtype: DTypeLike) -> None:
                 data=np.array([[1, 2, 3, 4, 5]]),
                 mask=np.array([[False, True, False, False, False]]),
             ),
+        },
+    )
+
+
+###############################################
+#     Tests for masked_select_along_batch     #
+###############################################
+
+
+def test_masked_select_along_batch_array() -> None:
+    assert objects_are_equal(
+        masked_select_along_batch(
+            np.array([[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]]),
+            mask=np.array([False, False, True, False, True]),
+        ),
+        np.array([[4, 5], [8, 9]]),
+    )
+
+
+def test_masked_select_along_batch_dict() -> None:
+    assert objects_are_equal(
+        masked_select_along_batch(
+            {
+                "a": np.array([[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]]),
+                "b": np.ma.masked_array(
+                    data=np.array([[5], [4], [3], [2], [1]]),
+                    mask=np.array([[False], [False], [False], [True], [False]]),
+                ),
+            },
+            mask=np.array([False, False, True, False, True]),
+        ),
+        {
+            "a": np.array([[4, 5], [8, 9]]),
+            "b": np.ma.masked_array(data=np.array([[3], [1]]), mask=np.array([[False], [False]])),
+        },
+    )
+
+
+#############################################
+#     Tests for masked_select_along_seq     #
+#############################################
+
+
+def test_masked_select_along_seq_array() -> None:
+    assert objects_are_equal(
+        masked_select_along_seq(
+            np.array([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]),
+            mask=np.array([False, False, True, False, True]),
+        ),
+        np.array([[2, 4], [7, 9]]),
+    )
+
+
+def test_masked_select_along_seq_dict() -> None:
+    assert objects_are_equal(
+        masked_select_along_seq(
+            {
+                "a": np.array([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]),
+                "b": np.ma.masked_array(
+                    data=np.array([[5, 4, 3, 2, 1]]),
+                    mask=np.array([[False, False, False, True, False]]),
+                ),
+            },
+            mask=np.array([False, False, True, False, True]),
+        ),
+        {
+            "a": np.array([[2, 4], [7, 9]]),
+            "b": np.ma.masked_array(data=np.array([[3, 1]]), mask=np.array([[False, False]])),
         },
     )
 
