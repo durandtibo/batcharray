1ed6ffbf996aad8b388a360a6b223c12ecd47ef7
Thibaut Durand <durand.tibo@gmail.com>
2024-04-20 21:53:18 -0700

    Add `nested.index_select_along_batch/seq`

--- src/batcharray/nested/indexing.py
+++ src/batcharray/nested/indexing.py
@@ -2,7 +2,12 @@ r"""Contain some array indexing functions for nested data."""
 
 from __future__ import annotations
 
-__all__ = ["take_along_batch", "take_along_seq"]
+__all__ = [
+    "index_select_along_batch",
+    "index_select_along_seq",
+    "take_along_batch",
+    "take_along_seq",
+]
 
 
 from functools import partial
@@ -15,6 +20,79 @@ if TYPE_CHECKING:
     import numpy as np
 
 T = TypeVar("T")
+
+
+def index_select_along_batch(data: Any, indices: np.ndarray) -> Any:
+    r"""Return the arrays which index the arrays along the batch axis
+    using the entries in ``indices``.
+
+    Note:
+        This function assumes the batch axis is the first
+            axis of the arrays. All the arrays should have the
+            same batch size.
+
+    Args:
+        data: The input data. Each item must be an array.
+        indices: The 1-D array containing the indices to take.
+
+    Returns:
+        The indexed arrays along the batch axis.
+
+    Example usage:
+
+    ```pycon
+
+    >>> import numpy as np
+    >>> from batcharray.nested import index_select_along_batch
+    >>> arrays = {
+    ...     "a": np.array([[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]]),
+    ...     "b": np.array([4, 3, 2, 1, 0]),
+    ... }
+    >>> out = index_select_along_batch(arrays, np.array([2, 4]))
+    >>> out
+    {'a': array([[4, 5], [8, 9]]), 'b': array([2, 0])}
+    >>> out = index_select_along_batch(arrays, np.array([4, 3, 2, 1, 0]))
+    >>> out
+    {'a': array([[8, 9], [6, 7], [4, 5], [2, 3], [0, 1]]), 'b': array([0, 1, 2, 3, 4])}
+
+    ```
+    """
+    return recursive_apply(data, partial(array.index_select_along_batch, indices=indices))
+
+
+def index_select_along_seq(data: Any, indices: np.ndarray) -> Any:
+    r"""Return the arrays which index the arrays along the sequence axis
+    using the entries in ``indices``.
+
+    Note:
+        This function assumes the sequence axis is the second
+            axis of the arrays. All the arrays should have the
+            same sequence size.
+
+    Args:
+        data: The input data. Each item must be an array.
+        indices: The 1-D array containing the indices to take.
+
+    Returns:
+        The indexed arrays along the sequence axis.
+
+    Example usage:
+
+    ```pycon
+
+    >>> import numpy as np
+    >>> from batcharray.nested import index_select_along_seq
+    >>> arrays = {'a': np.array([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]), 'b': np.array([[4, 3, 2, 1, 0]])}
+    >>> out = index_select_along_seq(arrays, np.array([2, 4]))
+    >>> out
+    {'a': array([[2, 4], [7, 9]]), 'b': array([[2, 0]])}
+    >>> out = index_select_along_seq(arrays, np.array([4, 3, 2, 1, 0]))
+    >>> out
+    {'a': array([[4, 3, 2, 1, 0], [9, 8, 7, 6, 5]]), 'b': array([[0, 1, 2, 3, 4]])}
+
+    ```
+    """
+    return recursive_apply(data, partial(array.index_select_along_seq, indices=indices))
 
 
 def take_along_batch(data: Any, indices: np.ndarray) -> Any:
