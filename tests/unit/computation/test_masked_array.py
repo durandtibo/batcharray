6fd08809d6cdd8783dccc5824a5118e7f25688df
Thibaut Durand <durand.tibo@gmail.com>
2024-06-19 16:59:54 -0700

    Add support for NumPy 2.0

--- tests/unit/computation/test_masked_array.py
+++ tests/unit/computation/test_masked_array.py
@@ -183,7 +183,9 @@ def test_masked_array_computation_model_argsort_axis_0(dtype: DTypeLike) -> None
     assert objects_are_equal(
         MaskedArrayComputationModel().argsort(
             np.ma.masked_array(
-                data=np.array([[3, 5, 0, 2, 4], [4, 7, 8, 8, 5], [8, 4, 1, 3, 0]], dtype=dtype),
+                data=np.array(
+                    [[1, 14, 4, 13, 6], [3, 0, 10, 5, 11], [2, 8, 7, 12, 9]], dtype=dtype
+                ),
                 mask=np.array(
                     [
                         [False, False, False, False, True],
@@ -194,12 +196,55 @@ def test_masked_array_computation_model_argsort_axis_0(dtype: DTypeLike) -> None
             ),
             axis=0,
         ),
+        np.array([[0, 1, 0, 2, 2], [2, 2, 1, 0, 1], [1, 0, 2, 1, 0]]),
+    )
+
+
+@pytest.mark.parametrize("dtype", DTYPES)
+def test_masked_array_computation_model_argsort_axis_0_stable(dtype: DTypeLike) -> None:
+    assert objects_are_equal(
+        MaskedArrayComputationModel().argsort(
+            np.ma.masked_array(
+                data=np.array([[3, 5, 0, 2, 4], [4, 7, 8, 8, 5], [8, 4, 1, 3, 0]], dtype=dtype),
+                mask=np.array(
+                    [
+                        [False, False, False, False, True],
+                        [False, False, False, True, False],
+                        [False, False, True, False, False],
+                    ]
+                ),
+            ),
+            axis=0,
+            kind="stable",
+        ),
         np.array([[0, 2, 0, 0, 2], [1, 0, 1, 2, 1], [2, 1, 2, 1, 0]]),
     )
 
 
 @pytest.mark.parametrize("dtype", DTYPES)
 def test_masked_array_computation_model_argsort_axis_1(dtype: DTypeLike) -> None:
+    assert objects_are_equal(
+        MaskedArrayComputationModel().argsort(
+            np.ma.masked_array(
+                data=np.array(
+                    [[1, 14, 4, 13, 6], [3, 0, 10, 5, 11], [2, 8, 7, 12, 9]], dtype=dtype
+                ),
+                mask=np.array(
+                    [
+                        [False, False, False, False, True],
+                        [False, False, False, True, False],
+                        [False, False, True, False, False],
+                    ]
+                ),
+            ),
+            axis=1,
+        ),
+        np.array([[0, 2, 3, 1, 4], [1, 0, 2, 4, 3], [0, 1, 4, 3, 2]]),
+    )
+
+
+@pytest.mark.parametrize("dtype", DTYPES)
+def test_masked_array_computation_model_argsort_axis_1_stable(dtype: DTypeLike) -> None:
     assert objects_are_equal(
         MaskedArrayComputationModel().argsort(
             np.ma.masked_array(
@@ -213,6 +258,7 @@ def test_masked_array_computation_model_argsort_axis_1(dtype: DTypeLike) -> None
                 ),
             ),
             axis=1,
+            kind="stable",
         ),
         np.array([[2, 3, 0, 1, 4], [0, 4, 1, 2, 3], [4, 1, 0, 3, 2]]),
     )
@@ -220,6 +266,27 @@ def test_masked_array_computation_model_argsort_axis_1(dtype: DTypeLike) -> None
 
 @pytest.mark.parametrize("dtype", DTYPES)
 def test_masked_array_computation_model_argsort_axis_none(dtype: DTypeLike) -> None:
+    assert objects_are_equal(
+        MaskedArrayComputationModel().argsort(
+            np.ma.masked_array(
+                data=np.array(
+                    [[1, 14, 4, 13, 6], [3, 0, 10, 5, 11], [2, 8, 7, 12, 9]], dtype=dtype
+                ),
+                mask=np.array(
+                    [
+                        [False, False, False, False, True],
+                        [False, False, False, True, False],
+                        [False, False, True, False, False],
+                    ]
+                ),
+            )
+        ),
+        np.array([6, 0, 10, 5, 2, 11, 14, 7, 9, 13, 3, 1, 4, 8, 12]),
+    )
+
+
+@pytest.mark.parametrize("dtype", DTYPES)
+def test_masked_array_computation_model_argsort_axis_none_stable(dtype: DTypeLike) -> None:
     assert objects_are_equal(
         MaskedArrayComputationModel().argsort(
             np.ma.masked_array(
@@ -231,7 +298,8 @@ def test_masked_array_computation_model_argsort_axis_none(dtype: DTypeLike) -> N
                         [False, False, True, False, False],
                     ]
                 ),
-            )
+            ),
+            kind="stable",
         ),
         np.array([2, 14, 3, 0, 5, 1, 9, 11, 6, 7, 10, 13, 4, 8, 12]),
     )
